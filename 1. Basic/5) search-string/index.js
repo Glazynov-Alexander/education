6c40938bee10a795b0d2a8029395bc46fe7d90d7
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-19 17:42:41 +0300

    search-string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,15 +1,37 @@
 function searchString(obj) {
   // Напиши свой код здесь
 
-let str = ``
-for(let s in obj ) {
-  str += s + obj[s] + '&' 
-}  
-  
-  return str;
+  let str = `?`;
+
+  Object.keys(obj).map((e) => {
+
+    if (typeof obj[e] === "number") {
+      obj[e] === 0 ? str += e + "=" + obj[e] :
+      str += `` + e + "=" + obj[e] + "&";
+    } 
+    else if (obj[e] === "") {
+       delete obj[e];
+    }
+    
+    else if (typeof obj[e] === "string") {
+      str += e + "=" + obj[e] ;
+    } 
+    
+    else if (obj[e] === "null") {
+      str += e + "=" + obj[e] + "&";
+    } 
+    
+    else if (obj[e] === "undefined") {
+      str += e + "=" + obj[e] + "&";
+    } 
+    else if (obj[e] === false) {
+      str +="&" + e + "=" + obj[e] + "&";
+    } 
+    
+  });
+  return Object.keys(obj).length == 0 ? '' : str;
 }
 
 window.searchString = searchString;
-
 
 export default searchString;
