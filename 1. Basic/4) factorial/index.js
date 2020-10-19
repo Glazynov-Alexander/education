e9c436d46ec5d7eb20417e8ebf8c655c777ea8d6
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-19 18:02:22 +0300

    factorial

--- 1. Basic/4) factorial/index.js
+++ 1. Basic/4) factorial/index.js
@@ -1,17 +1,14 @@
 function factorial(n) {
   // Напиши свой код здесь
 
+  let s = 1;
   if (n > 10) {
     return `Ошибка ${n} > 10 `;
-  } else {
-    for (let i = 0; i <= n; i++) {
-      let s = 1;
-      for (let j = 1; j <= n; j++) {
-        s = s * j ;
-      }
-      return s;
-    }
   }
+  for (let i = 1; i <= n; i++) {
+    s = s * i;
+  }
+  return s;
 }
 
 window.factorial = factorial;
