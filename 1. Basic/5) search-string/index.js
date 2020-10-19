cb72d1c4a36f3f6506988c171ca598ef655fdb1f
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-19 12:32:47 +0300

    TestBasic

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,7 +1,15 @@
 function searchString(obj) {
   // Напиши свой код здесь
+
+let str = ``
+for(let s in obj ) {
+  str += s + obj[s] + '&' 
+}  
+  
+  return str;
 }
 
 window.searchString = searchString;
+
 
 export default searchString;
