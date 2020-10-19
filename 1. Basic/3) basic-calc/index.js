4bc4004b813ff05f09579ed3399012b212a018bd
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-19 12:00:03 +0300

    TasksBasic

--- 1. Basic/3) basic-calc/index.js
+++ 1. Basic/3) basic-calc/index.js
@@ -1,5 +1,24 @@
 function basicCalc(operation, a, b) {
   // Напиши свой код здесь
+
+  switch(operation) {
+    case '+': {
+      return a + b
+    } 
+    case '-': {
+      return a - b
+    }
+
+    case '*' :{
+      return a * b
+    } 
+    case '/': {
+      return a / b
+    }
+
+    default : return console.log('null');
+  }
+
 }
 
 window.basicCalc = basicCalc;
--- 1. Basic/4) factorial/index.js
+++ 1. Basic/4) factorial/index.js
@@ -1,5 +1,17 @@
 function factorial(n) {
   // Напиши свой код здесь
+
+  if (n > 10) {
+    return `Ошибка ${n} > 10 `;
+  } else {
+    for (let i = 0; i <= n; i++) {
+      let s = 1;
+      for (let j = 1; j <= n; j++) {
+        s = s * j ;
+      }
+      return s
+    }
+  }
 }
 
 window.factorial = factorial;
