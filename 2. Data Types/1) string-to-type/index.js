3e07a59ac5c57a30adba38ac53d8d4ff83bdabe2
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-19 17:13:00 +0300

    string-to-type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,6 +1,30 @@
 function stringToType(str) {
   // Напиши свой код здесь
+
+  if (str === "undefined") {
+    return undefined;
+  } else if (str === "null") {
+    return null;
+  } else if(str === '') {
+    return ''
+  } else if(typeof(Number(str)) === "number") 
+  {
+    if(!isNaN(str) ) {
+      return Number(str)  
+    }
+    else if(str === "false" ) {
+      return false
+    } else if(str === "true") {
+      return true
+    }
+    else {
+      return str
+    }
+  } 
+  
+  
 };
+console.log(isNaN("2"));
 
 window.stringToType = stringToType;
 
