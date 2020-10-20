81ab1a7dbfbd5dfbdc72755110010fdaca53036d
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-20 15:29:23 +0300

    search-string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,14 +1,10 @@
 function searchString(obj) {
   // Напиши свой код здесь
-  let length = Object.keys(obj).length;
-  let str = "";
-  let sumStr = (key) => (str += key + "=" + obj[key] + "&");
-  let deleteObj = (key) => delete obj[key];
-
-  if(!Object.keys(obj).length) {
+  let mass = Object.keys(obj);
+  if(!mass.length) {
     return ''
   } 
-  let v = Object.keys(obj).reduce((a, key) => {
+  let v = mass.reduce((a, key) => {
     
     if (![undefined, null, ""].includes(obj[key])) {
        a.push(key + "=" + obj[key] )
