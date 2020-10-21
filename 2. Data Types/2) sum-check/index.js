a1bd65ca5cec2eec9dee2b887ff2648d13ad3783
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-21 15:56:32 +0300

    sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,15 +1,12 @@
 function sumCheck(a, b) {
   // Напиши свой код здесь
-  try {
-    if (typeof b === "number" && typeof a === "number") {
-      return a + b;
-    }
-    throw typeof a !== typeof b;
-  } catch (err) {
-    throw "Wrong arguments type!";
-  }
-}
 
+  if (typeof b === "number" && typeof a === "number") {
+    return a + b;
+  }
+
+  throw "Wrong arguments type!";
+}
 window.sumCheck = sumCheck;
 
 export default sumCheck;
