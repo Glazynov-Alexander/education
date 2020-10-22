af2cc5052c9a74dc9df63ca16b93538a881a2c91
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-22 11:25:36 +0300

    Array

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,17 @@
 function fullSum(...args) {
   // Напиши свой код здесь
+
+  let sum = 0;
+  if (args.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] !== "number") {
+      throw "Wrong Argument Type";
+    }
+    sum += args[i];
+  }
+  return sum;
 }
 
 window.fullSum = fullSum;
