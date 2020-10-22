c122b7e4f9930020af26cee9cf92f4a7bdbfd806
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-22 10:53:31 +0300

    is-equal-simple

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,5 +1,23 @@
 function isEqual(a, b) {
   // Напиши свой код здесь
+
+  if (a === null) {
+    return false;
+  } else {
+    let ad = Object.keys(a);
+    let bd = Object.keys(b);
+    for (let i = 0; i <= ad.length; i++) {
+      if (ad[i] === bd[i]) {
+        for (let j = 0; j <= ad.length; j++) {
+          if (typeof a[ad[j]] !== typeof b[bd[j]]) {
+            return false;
+          }
+        }
+        return true;
+      }
+      return false;
+    }
+  }
 }
 
 window.isEqual = isEqual;
