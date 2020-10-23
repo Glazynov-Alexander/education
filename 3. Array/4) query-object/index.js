2b447b31111e8eeed7ae0650222253247f36de08
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-23 10:10:40 +0300

    query-object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,6 +1,15 @@
+import stringToType from "../../2. Data Types/1) string-to-type/index.js";
 function queryToObject(query) {
   // Напиши свой код здесь
-};
+  let f = query.split("?").join("");
+  let obj = f.split("&").reduce((ob, v) => {
+    v = v.split("=");
+    let tip = stringToType(v[1]);
+    ob[v[0]] = tip;
+    return ob;
+  }, {});
+  return obj;
+}
 
 window.queryToObject = queryToObject;
 
