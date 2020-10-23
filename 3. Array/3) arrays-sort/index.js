89b8b34f48a08eecdc7c6bf3fc56dafdc5800dd9
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-23 16:01:14 +0300

    async-sum

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,10 +1,6 @@
 function arraysSort(arr) {
   // Напиши свой код здесь
-  return [...arr.sort((a, b) => {
-console.log(+a[a.length - 1], "dsa " + +b[b.length - 1]);
-
-   return +a[a.length - 1] - b[b.length - 1]
-  } )]
+  return [...arr.sort((a, b) => +a[a.length - 1] - b[b.length - 1])];
 }
 
 window.arraysSort = arraysSort;
