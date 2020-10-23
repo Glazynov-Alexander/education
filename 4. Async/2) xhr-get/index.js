987c3833b0962a1714939d627138fd165a599d6d
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-23 15:27:49 +0300

    xhr-get

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,21 @@
 function xhrGet(url) {
   // Напиши свой код здесь
+
+  return new Promise(async (resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.responseType = "json";
+
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      }
+      reject(xhr.response);
+    };
+
+    xhr.send();
+  });
 }
 
 window.xhrGet = xhrGet;
