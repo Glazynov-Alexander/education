3ca60a49b1eaae761c5313fddd0526e534e99310
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-26 15:54:04 +0300

    is-equal-advances

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,5 +1,31 @@
 function isEqualAdvances(a, b) {
   // Напиши свой код здесь
+  if (a === null || b === null) {
+    if (a === null && b === null) {
+      return true;
+    }
+    return false;
+  } else if (a === undefined || b === undefined) {
+    return false;
+  }
+
+  let aKeys = Object.keys(a);
+  let bKeys = Object.keys(b);
+  if (aKeys.length !== bKeys.length) {
+    return false;
+  }
+  for (let i = 0; i < aKeys.length; i++) {
+    if (bKeys.includes(aKeys[i]) === false) {
+      return false;
+    }
+  }
+  for (let j = 0; j < aKeys.length; j++) {
+    if (typeof a[aKeys[j]] === "object") {
+      return isEqualAdvances(a[aKeys[j]], b[aKeys[j]]);
+    }
+    return a[aKeys[j]] === b[bKeys[j]];
+  }
+  return true;
 }
 
 window.isEqualAdvances = isEqualAdvances;
