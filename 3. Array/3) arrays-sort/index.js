a6015585a6c7420059652aa3dea7fcdb516b7266
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-26 09:04:33 +0300

    s

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,6 +1,6 @@
 function arraysSort(arr) {
   // Напиши свой код здесь
- arr.sort((a, b) => console.log(a+ "  " + b +"  " + arr ))
+
   return [...arr.sort((a, b) => a[a.length - 1] - b[b.length - 1] )];
 }
 
