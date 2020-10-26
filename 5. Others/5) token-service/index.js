7de61de9da04d3c4aca4e1332308c9349ea4d3b9
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-10-26 09:46:50 +0300

    token-service

--- 5. Others/5) token-service/index.js
+++ 5. Others/5) token-service/index.js
@@ -9,6 +9,31 @@
 
 class TokenService {
   // Напиши свой код здесь
+  constructor() {
+    this.f = null;
+    this.mass = [];
+  }
+
+  subscribe(newToken) {
+    this.mass.push(newToken);
+  }
+
+  setToken(token) {
+    this.mass.forEach((e) => {
+      e(token);
+    });
+    this.f = token;
+  }
+
+  removeToken() {
+    this.mass.forEach((e) => {
+      e(this.f);
+    });
+  }
+
+  getToken() {
+    return this.f;
+  }
 }
 
 window.TokenService = TokenService;
